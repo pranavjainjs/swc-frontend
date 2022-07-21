9c17f2e9948792a1893015e2b03b069b33b14842
Pranav <95164530+thefakepranav@users.noreply.github.com>
2022-07-21 22:30:23 +0530

    added add feature

--- task2/script.js
+++ task2/script.js
@@ -4,6 +4,7 @@ const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const searchURL = BASE_URL + '/search/movie?' + API_KEY;
 var list;
+var flag = 0;
 
 fetch(API_URL)
     .then(response => response.json())
@@ -24,28 +25,38 @@ async function defaultList(){
 // document.getElementById('quick_search').addEventListener('keydown', search);
 
 function assignData(list) {
-    let i=0
+    let i=0;
     for(; i<list.length; i++){
         const card = document.createElement('div');
         const img = document.createElement('img');
         const title = document.createElement('h2');
-        const rating = document.createElement('p');        
+        const rating = document.createElement('p'); 
+        const link = 'https://www.imdb.com/find?q=' + list[i].title.replace(/ /g, '+') + '&ref_=nv_sr_sm';
+        const texttt = document.createElement('p');
+        texttt.innerText = link;
+        // const desc = document.createElement('div');
+        // const overview = document.createElement('p');
 
-        card.classList.add("text-center", 'rounded-lg', 'bg-gray-200', 'shadow-lg', 'w-80', 'p-2');
-        img.classList.add('opacity-100', 'hover:opacity-80', "w-64", 'm-auto', 'p-2', 'pb-2');
-        title.classList.add('pt-2', 'text-xl', 'font-serif')
-
-        // card.style.backgroundImage = 'url(${IMG_URL} + ${list[0].poster_path})'
+        card.classList.add("text-center", 'rounded-lg', 'bg-gray-200', 'shadow-lg', 'w-80', 'p-2', 'relative', 'hover:scale-100', 'ease-in', 'duration-300', 'hover:drop-shadow-xl', 'scale-95');
+        img.classList.add('opacity-100', 'hover:opacity-80', "w-64", 'm-auto', 'p-2', 'pb-2', 'hover:scale-105', 'ease-in', 'duration-300');
+        title.classList.add('pt-2', 'text-xl', 'font-serif');
+        // desc.classList.add('w-64', 'absolute', 'bg-black', 'text-white');
 
         card.appendChild(img);
         card.appendChild(title);
         card.appendChild(rating);
         document.getElementById("bunch").appendChild(card);
+        // desc.appendChild(overview);
+        
+        // card.addEventListener('hover', displayDesc);
+        title.onclick = () => window.open(link);
+        title.onmouseover = () => title.classList.add('cursor-pointer')
+        // title.onclick = () => console.log(link);
 
         img.src = IMG_URL + list[i].poster_path;
         title.innerText = list[i].title;
         rating.innerText = list[i].vote_average + '\n (' + list[i].vote_count + ' votes)';
-
+        // overview.innerText = list[i].overview;
     }
     if(i < 3){
             document.getElementById('container').classList.remove('h-auto');
@@ -74,10 +85,36 @@ async function search2(){
     mysearchresult = await mysearchresult.json();
     // console.log(mysearchresult);
     assignData(mysearchresult.results);
+    flag = 1;
     return;
 }
 
+const addbtn = document.getElementById('add');
+var page = 2;
 
+addbtn.onclick = () => {
+    if(flag === 0){
+        const API_URL_PAGE = API_URL + "&&page=" + page;
+        addMovies(API_URL_PAGE, page);
+        page++;
+    } else{
+        let query = document.getElementById('quick_search').value.trim().toLowerCase();
+        const searchQueryURL = searchURL+'&query='+query;
+        const searchNextPage = searchQueryURL + "&&page=" + page;
+        addMovies(searchNextPage, page);
+        page++;
+    }
+}
+
+function addMovies(link, page){
+    fetch(link)
+    .then(response => response.json())
+    .then(data => {
+        // console.log(data);
+        list = data.results;
+        assignData(list);
+    })
+}
 
 /*
 
